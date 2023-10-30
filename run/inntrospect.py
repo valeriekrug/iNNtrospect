3a3a65be570de8b0a9061076caecdafff9806fd7
Valerie Krug <valerie.krug@ovgu.de>
2023-10-30 10:16:14 +0100

    config-based preprocessing

--- run/inntrospect.py
+++ run/inntrospect.py
@@ -1,51 +1,6 @@
-# interactive notebook for running a pipeline with interactive prompts on command line
+# script for performing iNNtrospection
 # fixed instances of this script go to examples
 
-from env_setup import ignore_user_installs, set_active_GPU
-from src.data_processing import process_corpus_file, align_data
-
-from src.model import load_model, create_model_with_layer_outputs
-
-import os
-import numpy as np
-
-from src.naps import compute_naps
-
-ignore_user_installs('akrug')
-set_active_GPU(0)
-
-# "config" dummies
-# model_path = '/data/project/ANNalyzer/models/MNIST_MLP'
-# layer_names_of_interest = ['layer_2']
-# data_path = '/data/project/iNNtrospect/MNIST/'
-# processed_corpus_path = '../output/MNIST_MLP'
-
-
-model_path = '/data/project/ANNalyzer/models/MNIST_CNN_SHALLOW'
-layer_names_of_interest = ['conv2d_24','conv2d_25','flatten_8']
-data_path = '/data/project/iNNtrospect/MNIST/'
-processed_corpus_path = '../output/MNIST_CNN'
-
-# # load model
-# model = load_model(model_path)
-#
-# # make model with output layers of interest
-# model_with_layer_outputs = create_model_with_layer_outputs(model, layer_names_of_interest)
-#
-# # provide (batches) of examples with group annotation
-# # format:
-# # filename, groupname, (expected output)
-# # batch1.npy, shirt, (0)
-# # batch2.npy, shirt, (0)
-# # batch3.npy, trouser, (1)
-#
-# process_corpus_file(data_path,
-#                     model_with_layer_outputs,
-#                     processed_corpus_path)
-#
-# align_data(processed_corpus_path)
-
-compute_naps(processed_corpus_path)
 
 # TODO contrastive NAPs for a user-selected subset of groups
 
---
+++ run/preprocess.py
@@ -0,0 +1,50 @@
+# script for running a preprocessing pipeline to generate data for iNNtrospection
+# fixed instances of this script go to examples
+import json
+
+from env_setup import ignore_user_installs, set_active_GPU
+from src.data_processing import process_corpus_file, align_data
+from src.model import load_model, create_model_with_layer_outputs
+from src.naps import compute_naps
+
+ignore_user_installs('akrug')
+set_active_GPU(0)
+
+# config_file = "../configs/MNIST_MLP.json"
+config_file = "../configs/MNIST_CNN_SHALLOW.json"
+with open(config_file, "r") as f:
+    config = json.load(f)
+
+
+# load model
+model = load_model(config["model_path"])
+
+# make model with output layers of interest
+model_with_layer_outputs = create_model_with_layer_outputs(model,
+                                                           config["layer_names_of_interest"])
+
+# provide (batches) of examples with group annotation
+# format:
+# filename, groupname, (expected output)
+# batch1.npy, shirt, (0)
+# batch2.npy, shirt, (0)
+# batch3.npy, trouser, (1)
+
+# computing activations and gradients
+# saved to <processed_corpus_path>/acts/ and <processed_corpus_path>/grads/
+process_corpus_file(config["data_path"],
+                    model_with_layer_outputs,
+                    config["processed_corpus_path"])
+
+# creating aligned activations in every layer where possible
+# saved to <processed_corpus_path>/aligned/
+if config["use_aligned_naps"]:
+    align_data(config["processed_corpus_path"])
+
+# compute group averages in each layer
+# saved to <processed_corpus_path>/naps/
+compute_naps(config["processed_corpus_path"], use_aligned_acts=config["use_aligned_naps"])
+
+
+
+
